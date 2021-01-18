f5b3c279f6967e7b3745d6e6350043ab912f7f3c
miyakeylab <miyasaviola@gmail.com>
2021-01-18 09:50:22 +0900

    修正

--- slack_app_test/app/Http/Controllers/EventController.php
+++ slack_app_test/app/Http/Controllers/EventController.php
@@ -11,12 +11,13 @@ class EventController extends Controller
      * @param Request $request
      * @return false|string
      */
-    public function index()
+    public function index(Request $request)
     {
 //        logger(__CLASS__ . __FUNCTION__);
-//        logger($request->all());
+        $req = $request->all();
         Log::error(__CLASS__ . __FUNCTION__);
-        return json_encode([]);
+
+        return json_encode([$req['challenge']]);
     }
 
 }
