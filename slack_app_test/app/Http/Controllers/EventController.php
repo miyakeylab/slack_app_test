b40d3ce7758dbbf4ee682a026bf887cdd73e3e00
miyakeylab <miyasaviola@gmail.com>
2021-03-16 18:59:08 +0900

    修正

--- slack_app_test/app/Http/Controllers/EventController.php
+++ slack_app_test/app/Http/Controllers/EventController.php
@@ -17,12 +17,28 @@ class EventController extends Controller
         $req = $request->all();
         Log::info($req);
 
-        if(isset($req['challenge'])) {
+        if (isset($req['challenge'])) {
             return json_encode([$req['challenge']]);
-        }else{
-            if(isset($req['type']) &&$req['type'] == "emoji_changed")
-            {
-                Log::info('emoji_change');
+        } else {
+            if(isset($req['type'])&& $req['type'] == "event_callback") {
+                Log::info('event_callback');
+                if (isset($req['event']['type']) && $req['event']['type'] == "emoji_changed") {
+                    if ($req['event']['subtype'] == "add") {
+                        $name = $req['event']["name"];
+                        $text = "{$name}の絵文字が追加されました！\n\n :{$name}: ";
+
+                    } else if ($req['event']['subtype'] == "remove") {
+                        $names = $req['event']["names"];
+                        $icons = "";
+                        foreach ($names as $name) {
+                            $icons .= "\n " . $name;
+                        }
+
+                        $text = "絵文字がなくなっちゃいました :cry:\n\n {$icons}";
+                    }
+
+                    Log::info('emoji_change');
+                }
             }
             return;
         }
